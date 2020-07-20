747faa6046f8404a8da9dbc6de9b2f96d19a4b8c
arjun344 <karjun344@gmail.com>
2020-07-20 23:54:19 +0530

    Initialized LiveWeb

---
+++ foreground.js
@@ -0,0 +1,11 @@
+console.log("From Foreground");
+try {
+	document.querySelector('#hplogo').classList.add('spinspinspin');
+}
+catch {
+
+document.querySelector('.logo img').classList.add('spinspinspin');
+
+}
+
+
